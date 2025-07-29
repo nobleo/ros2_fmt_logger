da592e2ea5bd33c2e5a894cae32ccddd13bf1ada
Tim Clephas <tim.clephas@nobleo.nl>
2025-07-29 21:46:27 +0200

    Syntax

--- include/ros2_fmt_logger/ros2_fmt_logger.hpp
+++ include/ros2_fmt_logger/ros2_fmt_logger.hpp
@@ -28,7 +28,7 @@ struct format_string
 class Logger
 {
 public:
-  explicit Logger(const rclcpp::Logger & logger) : rcllog_(logger) {}
+  explicit Logger(const rclcpp::Logger & logger) : rcl_logger_(logger) {}
 
   template <typename... Args>
   void fatal(const format_string & format, Args &&... args) const
@@ -37,17 +37,18 @@ public:
   }
 
 private:
-  rclcpp::Logger rcllog_;
+  rclcpp::Logger rcl_logger_;
 
   void _fatal(const format_string & format, fmt::format_args args) const
   {
-    if (rcutils_logging_logger_is_enabled_for(rcllog_.get_name(), RCUTILS_LOG_SEVERITY_FATAL)) {
-      std::string formatted_msg = fmt::vformat(format.str, args);
-      rcutils_log_location_t rcutils_location = {
-        format.loc.function_name(), format.loc.file_name(), static_cast<size_t>(format.loc.line())};
+    if (rcutils_logging_logger_is_enabled_for(rcl_logger_.get_name(), RCUTILS_LOG_SEVERITY_FATAL)) {
+      rcutils_log_location_t rcutils_location{
+        .function_name = format.loc.function_name(),
+        .file_name = format.loc.file_name(),
+        .line_number = static_cast<size_t>(format.loc.line())};
       rcutils_log(
-        &rcutils_location, RCUTILS_LOG_SEVERITY_FATAL, rcllog_.get_name(), "%s",
-        formatted_msg.c_str());
+        &rcutils_location, RCUTILS_LOG_SEVERITY_FATAL, rcl_logger_.get_name(), "%s",
+        fmt::vformat(format.str, args).c_str());
     }
   }
 };
