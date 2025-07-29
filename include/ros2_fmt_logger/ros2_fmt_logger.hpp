5a721f06439f752523a798c4653c336c9a8cc406
Tim Clephas <tim.clephas@nobleo.nl>
2025-07-29 17:18:17 +0200

    Start with fatal

--- include/ros2_fmt_logger/ros2_fmt_logger.hpp
+++ include/ros2_fmt_logger/ros2_fmt_logger.hpp
@@ -1,3 +1,21 @@
 // Copyright (C) 2025 Nobleo Autonomous Solutions B.V.
 
 #pragma once
+
+#include <rclcpp/logger.hpp>
+
+namespace ros2_fmt_logger
+{
+class Logger
+{
+public:
+  explicit Logger(const rclcpp::Logger & logger) : rcllog_(logger) {}
+
+  //   RCLCPP_FATAL(
+  //   old_logger_, "No messages or signals retrieved from dbc-file: %s", HRI_dbcFile_.c_str());
+  // logger_.fatal("No messages or signals retrieved from dbc-file: {}", HRI_dbcFile_);
+
+private:
+  rclcpp::Logger rcllog_;
+};
+}  // namespace ros2_fmt_logger
