103651a8a54325193064e36bbd011d759c06301f
Tim Clephas <tim.clephas@nobleo.nl>
2026-02-24 11:35:17 +0100

    Backwards compatibility 'all headers'

---
+++ include/ros2_fmt_logger/ros2_fmt_logger.hpp
@@ -0,0 +1,10 @@
+// Copyright (C) 2026 Nobleo Autonomous Solutions B.V.
+
+#pragma once
+
+// This header serves as the main include for the ros2_fmt_logger package,
+// providing a single entry point for users to include all the loggers functionality.
+// Users can opt to include specific headers if they only need certain features.
+
+#include "ros2_fmt_logger/logger.hpp"
+#include "ros2_fmt_logger/rclcpp_formatters.hpp"
